c0a53350abe633e5e228a060767255c3d1f6adb3
isaacwach <isaacwach29@gmail.com>
2021-12-20 13:14:57 +0300

    added functions to toggle between development and product icons, and their description

--- index.js
+++ index.js
@@ -7,4 +7,20 @@ $(document).ready(function() {
         $(".design-text").hide("1000");
         $(".design-icon").show("1000");
     })
+    $(".dev-icon").click(function(){
+        $(".dev-icon").slideDown("1500").hide("1000");
+        $(".dev-text").show("1000");
+    })
+    $(".dev-text").click(function(){
+        $(".dev-text").hide("1000");
+        $(".dev-icon").show("1000");
+    })
+    $(".product-icon").click(function(){
+        $(".product-icon").slideDown("1500").hide("1000");
+        $(".product-text").show("1000");
+    })
+    $(".product-text").click(function(){
+        $(".product-text").hide("1000");
+        $(".product-icon").show("1000");
+    })
 })
