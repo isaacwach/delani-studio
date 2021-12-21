f6159e8e30e11b14add5273aeb0f8c85aad1a140
isaacwach <isaacwach29@gmail.com>
2021-12-21 11:20:55 +0300

    added a hover effect to the portfolio images

--- index.js
+++ index.js
@@ -23,4 +23,54 @@ $(document).ready(function() {
         $(".product-text").hide("1000");
         $(".product-icon").show("1000");
     })
+
+
+    $(".image").mouseover(function(){
+        $("#hidden1").show()
+    })
+    $(".image").mouseout(function(){
+        $("#hidden1").hide()
+    })
+    $(".image1").mouseover(function(){
+        $("#hidden2").show()
+    })
+    $(".image1").mouseout(function(){
+        $("#hidden2").hide()
+    })
+    $(".image2").mouseover(function(){
+        $("#hidden3").show()
+    })
+    $(".image2").mouseout(function(){
+        $("#hidden3").hide()
+    })
+    $(".image3").mouseover(function(){
+        $("#hidden4").show()
+    })
+    $(".image3").mouseout(function(){
+        $("#hidden4").hide()
+    })
+    $(".image4").mouseover(function(){
+        $("#hidden5").show()
+    })
+    $(".image4").mouseout(function(){
+        $("#hidden5").hide()
+    })
+    $(".image5").mouseover(function(){
+        $("#hidden6").show()
+    })
+    $(".image5").mouseout(function(){
+        $("#hidden6").hide()
+    })
+    $(".image6").mouseover(function(){
+        $("#hidden7").show()
+    })
+    $(".image6").mouseout(function(){
+        $("#hidden7").hide()
+    })
+    $(".image7").mouseover(function(){
+        $("#hidden8").show()
+    })
+    $(".image7").mouseout(function(){
+        $("#hidden8").hide()
+    })
 })
